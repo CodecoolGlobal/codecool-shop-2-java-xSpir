1f7279a69134052fca899a6d802dacc44e3c6dc2
KizsGergely <hu.gergely@gmail.com>
2021-06-17 20:28:06 +0200

    Add even more content

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -40,8 +40,8 @@ public class Initializer implements ServletContextListener {
 
 
         //setting up a new product category
-        ProductCategory tablet = new ProductCategory(1, "Linux kernel", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        productCategoryDataStore.add(tablet);
+        ProductCategory kernel = new ProductCategory(1, "Linux kernel", "Hardware", "The world's most popular open-source operating systems.");
+        productCategoryDataStore.add(kernel);
 
         ProductCategory edibleNuts = new ProductCategory(2, "Edible Nut", "Nut", "A nut you can eat.");
         productCategoryDataStore.add(edibleNuts);
@@ -53,11 +53,25 @@ public class Initializer implements ServletContextListener {
         productCategoryDataStore.add(nutrients);
 
         //setting up products and printing it
-        productDataStore.add(new Product(1,"Amazon Fire", 49.9f, "USD", "De String.", tablet, amazon));
-        productDataStore.add(new Product(2,"Lenovo IdeaPad Miix 700", 479, "USD", "Description String.", tablet, lenovo));
-        productDataStore.add(new Product(3,"Amazon Randomtablet", 49.9f, "USD", "Description String.", tablet, amazon));
-        productDataStore.add(new Product(4,"Nuts", 49.9f, "USD", "Description String.", edibleNuts, amazon));
-        productDataStore.add(new Product(4,"Nuts", 49.9f, "USD", "Description String.", edibleNuts, amazon));
+        productDataStore.add(new Product(1,"Linux Kernel 4.13", 1149.9f, "USD", "The stuff with your favourite penguin sliding game.", kernel, linux));
+        productDataStore.add(new Product(2,"Linux Kernel 5.0", 979.9f, "USD", "Szamitson szojatekra.", kernel, linux));
+        productDataStore.add(new Product(3,"Linux Kernel 5.8", 666.6f, "USD", "Good as hell!", kernel, linux));
+        productDataStore.add(new Product(4,"Linux Kernel 5.12", 44.4f, "USD", "Fresh and reliable as a nut.", kernel, linux));
+
+        productDataStore.add(new Product(5,"Regular nut", 249.9f, "USD", "For regular occasions.", edibleNuts, nutKing));
+        productDataStore.add(new Product(6,"Christmas nut", 4345.9f, "USD", "Share with your family.", edibleNuts, nutKing));
+        productDataStore.add(new Product(7,"Oily nut", 3429.9f, "USD", "Good for your skin.", edibleNuts, nutKing));
+        productDataStore.add(new Product(8,"Misterious Nutto", 1000.9f, "USD", "Try me - Misterious Nutto", edibleNuts, nutKing));
+
+        productDataStore.add(new Product(9,"Type 1 Nuclear Core", 300.9f, "USD", "Don't ask me.", nuclearCore, paks2));
+        productDataStore.add(new Product(10,"Not so Xplody Core", 475900.9f, "USD", "For the nation.", nuclearCore, paks2));
+        productDataStore.add(new Product(11,"Leaky Core", 26546.9f, "USD", "Don't ask, don't tell.", nuclearCore, paks2));
+        productDataStore.add(new Product(12,"Indestructible core", 1000.9f, "USD", "Barely used.", nuclearCore, paks2));
+
+        productDataStore.add(new Product(13,"Whey", 469.1f, "USD", "This the whey.", nutrients, gymBro));
+        productDataStore.add(new Product(14,"Grains", 649.1f, "USD", "This is the grai...it doesn't work.", nutrients, gymBro));
+        productDataStore.add(new Product(15,"Vegetables", 496.1f, "USD", "It's more likely a food, than a table.", nutrients, gymBro));
+        productDataStore.add(new Product(16,"Fruits", 694.1f, "USD", "Smooth some juice!", nutrients, gymBro));
 
     }
 }
