e4b0aa140632a9e1d887c627a58e629af12c98d9
Plantelo <trabantkedvelo@gmail.com>
2021-06-18 09:57:09 +0200

    Cleanup of alert.js

--- src/main/webapp/static/js/alert.js
+++ src/main/webapp/static/js/alert.js
@@ -1,15 +1,14 @@
 if (document.URL.match("[\?]")) {
     let alertParams = new URLSearchParams(document.URL.split('?')[1]);
 
-    console.log(alertParams.get("redir"));
-    console.log(alertParams.keys());
-
     switch (alertParams.get("redir")) {
         case "payment_fail":
             alert("Payment unsuccessful due to invalid payment data or insufficient funds");
             break;
         case "empty_cart":
             alert("You may not pay with an empty cart");
+            break;
+        case null:
             break;
         default:
             alert("Invalid redirect");
