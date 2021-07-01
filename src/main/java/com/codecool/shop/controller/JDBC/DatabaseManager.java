ea80e7893fc8c99e400aedaadde54423ad3e2bee
balintovics <molnar99b@gmail.com>
2021-07-01 09:59:08 +0200

    feat: all queries written in corresponding classes

--- src/main/java/com/codecool/shop/controller/JDBC/DatabaseManager.java
+++ src/main/java/com/codecool/shop/controller/JDBC/DatabaseManager.java
@@ -1,6 +1,9 @@
 package com.codecool.shop.controller.JDBC;
 
+import com.codecool.shop.dao.implementation.ProductDaoJDBC;
 import org.postgresql.ds.PGSimpleDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -11,6 +14,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseManager {
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseManager.class);
+    private static Object dao;
+
     public DataSource connect() throws SQLException {
         String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
         String appConfigPath = rootPath + "connection.properties";
@@ -19,8 +25,9 @@ public class DatabaseManager {
         Properties appProps = new Properties();
         try {
             appProps.load(new FileInputStream(appConfigPath));
-            System.out.println("appProps = " + appProps);
-        } catch (IOException e) { //TODO: logger
+            dao = appProps.get("dao");
+        } catch (IOException e) {
+            logger.info("IO Exception while searching for properties file: " + e.getMessage());
             e.printStackTrace();
         }
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
@@ -33,5 +40,9 @@ public class DatabaseManager {
         System.out.println("Connection OK");
 
         return dataSource;
+    }
+
+    public static Object getDao() {
+        return dao;
     }
 }
